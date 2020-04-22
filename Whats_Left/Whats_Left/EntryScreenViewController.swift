eb34591c6d0c3b698ba92086cebdebdfadf8e427
Gayeon Park <gayeon.park@nyu.edu>
2020-04-22 19:44:44 -0400

    first screen done added constraints and linked to tableview

---
+++ Whats_Left/Whats_Left/EntryScreenViewController.swift
@@ -0,0 +1,33 @@
+//
+//  EntryScreenViewController.swift
+//  Whats_Left
+//
+//  Created by Gayeon Park on 4/22/20.
+//  Copyright © 2020 daniel. All rights reserved.
+//
+
+import UIKit
+
+class EntryScreenViewController: UIViewController {
+
+    @IBOutlet weak var onButtonClick: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
